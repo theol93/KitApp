f5ffc3828dddfd84b5bd8bf99053cea81cca7f93
Dmytro Chornovol <dbtheoldb@gmail.com>
2026-03-05 19:46:44 +0200

    remove unused code & libs

--- src/core/firebase/index.ts
+++ src/core/firebase/index.ts
@@ -1,27 +1,19 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { initializeFirestore, persistentLocalCache } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 import { Platform } from 'react-native';
-import Config from "react-native-config";
+import Config from 'react-native-config';
 
 const firebaseConfig = {
-    apiKey: Config.FIREBASE_API_KEY,
-    authDomain: Config.FIREBASE_AUTH_DOMAIN,
-    projectId: Config.FIREBASE_PROJECT_ID,
-    storageBucket: Config.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
-    appId: Platform.OS === 'ios' ? Config.FIREBASE_APP_ID_IOS : Config.FIREBASE_APP_ID_ANDROID,
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Platform.OS === 'ios' ? Config.FIREBASE_APP_ID_IOS : Config.FIREBASE_APP_ID_ANDROID,
 };
 
-
-const app = getApps().length === 0
-    ? initializeApp(firebaseConfig)
-    : getApp();
-
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const db = initializeFirestore(app, {
-    localCache: persistentLocalCache(),
+  localCache: persistentLocalCache(),
 });
-export const storage = getStorage(app);
-
-
--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,17 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import tasksReducer from './tasks/tasksReducer';
-import settingsReducer from './settings/settingsReducer';
 import { firestoreApi } from '../api/firestoreApi';
-
 
 export const store = configureStore({
   reducer: {
-    tasks: tasksReducer,
-    settings: settingsReducer,
     [firestoreApi.reducerPath]: firestoreApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(firestoreApi.middleware),
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(firestoreApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
